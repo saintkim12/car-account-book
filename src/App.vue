8a0422925459d013d2f7f51031e7f8e3163fb962
saintkim12 <22ini@naver.com>
2023-02-02 21:11:45 +0900

    페이지 전환 작업중

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import localforage from 'localforage'
 import AddItem from './components/AddItem.vue'
 import DisplayItem from './components/DisplayItem.vue'
 import AppConfig from './components/AppConfig.vue'
+import Main from './components/Main.vue'
 
 export type CarAccountItem = Record<string, any>
 
@@ -24,17 +25,34 @@ onMounted(async () => {
   } catch (e) {
     console.error(e)
   }
-  watch(items, (v) => {
-    console.log('items', JSON.parse(JSON.stringify(v)))
-    localforage.setItem('items', JSON.parse(JSON.stringify(v))).then(async () => {
-      console.log('items', (await localforage.getItem<CarAccountItem[]>('items')))
-    })
-  }, { deep: true })
+  watch(
+    items,
+    (v) => {
+      console.log('items', JSON.parse(JSON.stringify(v)))
+      localforage.setItem('items', JSON.parse(JSON.stringify(v))).then(async () => {
+        console.log('items', await localforage.getItem<CarAccountItem[]>('items'))
+      })
+    },
+    { deep: true }
+  )
 })
 </script>
 
 <template>
-  <div class="block">
+  <template v-if="page === 'addItem'">
+    <AddItem v-bind="{}" @add="addItem"></AddItem>
+  </template>
+  <template v-else-if="page === 'displayItem'">
+    <DisplayItem v-bind="{ items }"></DisplayItem>
+  </template>
+  <template v-else-if="page === 'appConfig'">
+    <AppConfig v-bind="{}"></AppConfig>
+  </template>
+  <template v-else>
+    <Main @update-page="(p) => page = p" />
+  </template>
+  <!-- @NOTE: 감춤 -->
+  <div v-if="false" class="block">
     <div class="columns">
       <div class="column is-2">
         <button class="button" @click="page = 'addItem'">AddItem</button>
@@ -71,11 +89,16 @@ onMounted(async () => {
 @import 'bulma/bulma.sass';
 </style>
 <style>
-html, body, #app {
+html,
+body,
+#app {
   width: 100%;
   height: 100%;
 }
-html, body, #app {
+
+html,
+body,
+#app {
   margin: 0;
   padding: 0;
 }
---
+++ src/components/Main.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div class="block" style="position:relative;top:50%;">
+    <div class="columns is-multiline">
+      <div class="column is-full"><button class="button is-fullwidth" @click="$emit('update-page', 'addItem')">항목 등록</button></div>
+      <div class="column is-full"><button class="button is-fullwidth" @click="$emit('update-page', 'displayItem')">항목 조회</button></div>
+      <div class="column is-full"><button class="button is-fullwidth" @click="$emit('update-page', 'appConfig')">설정</button></div>
+    </div>
+  </div>
+</template>
